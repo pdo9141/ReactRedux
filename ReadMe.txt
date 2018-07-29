ddec30011eb59a68329fa9d803030eb94e1c845b
pdo9141 <pdo9141@gmail.com>
2018-07-29 14:45:04 -0700

    Adding final redux tutorial files

--- ReadMe.txt
+++ ReadMe.txt
@@ -2,3 +2,9 @@
 02) Install yarn: "npm install -g yarn"
 03) CD into redux-tutorial: "yarn add redux react-redux"
 04) Start app: "yarn start"
+05) Install Redux DevTools Chrome extension
+06) Add line "window.devToolsExtension && window.devToolsExtension()" to createStore function to talk to Redux DevTools
+07) To make API requests: "yarn add redux-thunk jquery"
+08) For performance add Reselect: "yarn add reselect"
+09) Consider Store > Component > Sub-component pattern (Smart vs dumb components). Keeps things cleaner with fewer components connected to store
+    State flows down to dumb components. E.g., Store > productsPage > addProduct or productsList (dumb)
--- redux-tutorial/src/App.js
+++ redux-tutorial/src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { updateUser, apiRequest } from './actions/user-actions';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.onUpdateUser = this.onUpdateUser.bind(this);
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.props.onApiRequest();
+    }, 1500);
+  }
+
+  onUpdateUser(event) {
+    if (event && event.target && event.target.value) {
+      this.props.onUpdateUser(event.target.value);
+    }
+    else {
+      this.props.onUpdateUser('Sammy');
+    }
+  }
+
   render() {
+    // console.log(this.props);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -13,9 +38,39 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div onClick={this.onUpdateUser}>Update user</div><br />
+        {this.props.user}<br />
+        <input onChange={this.onUpdateUser} />
       </div>
     );
   }
 }
 
-export default App;
+const mapStateToProps = (state, props) => {
+  console.log('In mapStateToProps: ', props);
+
+  return {
+    products: state.products,
+    user: state.user,
+    userPlusProp: `${state.user} ${props.aRandomProps}`
+  };
+};
+
+const mapActionsToProps = (dispatch, props) => {
+  console.log('In mapActionsToProps: ', props);
+
+  return bindActionCreators({
+    onUpdateUser: updateUser,
+    onApiRequest: apiRequest
+  }, dispatch);
+};
+
+// const mergeProps = (propsFromState, propsFromDispatch, ownProps) => {
+//   console.log('propsFromState: ', propsFromState);
+//   console.log('propsFromDispatch: ', propsFromDispatch);
+//   console.log('ownProps: ', ownProps);
+//   return {};
+// };
+
+// export default connect(mapStateToProps, mapActionsToProps, mergeProps)(App);
+export default connect(mapStateToProps, mapActionsToProps)(App);
